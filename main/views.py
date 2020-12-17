9cd09ae4018f38cd28a5e83ec2942d2db76deea4
Matt Mont <mattmont415@gmail.com>
2020-12-16 20:56:23 -0800

    E-mail sends to other gmail accounts! Without having to do the 'lessecureapps' feature with other accounts - shocked

--- main/views.py
+++ main/views.py
@@ -20,15 +20,22 @@ def order(request):
 
     #Somewhere check if the REQUEST value is > 0, then put in e-mail
 
-    message = "You ordered " + request.POST['personal'] + " personal meals.\n" + "And " + request.POST['family'] + " family meals." + "The address to send is " + request.POST['address']
+    message = "Thank you for ordering with us!\n"
+    message += "You ordered: \n"
+    # if request.POST['namea'].value > 0:
+    #   message += nameA + "\n"
+    #   message += "Quantity: " + request.POST['namea'].value + "\n"
+    #   message += "Total for item: " + (request.POST['namea'] * priceA)
     # personal_meal = request.POST['personal']
     # family_meal = request.POST['family']
+    message += "Your order will come to: \n"
+    message += request.POST['address']
 
     send_mail(
       message_name, #Subject
       message, #Message itself
       "tonisfabkitchen@gmail.com", #from email
-      ['mattmont415@gmail.com', 'tonisfabkitchen@gmail.com', message_email], #to email
+      ['mattmont415@gmail.com', 'alexchen9333@gmail.com', 'tonisfabkitchen@gmail.com', message_email], #to email
     )
 
     return render(request, "main/send_email.html")
@@ -45,58 +52,62 @@ def admin5959(request):
   if request.method == "POST":
     #Set the variable to the name of what each slot is
     namea = request.POST['namea']
-    nameb = request.POST['nameb']
-    namec = request.POST['namec']
-    named = request.POST['named']
-    namee = request.POST['namee']
-    namef = request.POST['namef']
-    nameg = request.POST['nameg']
-    nameh = request.POST['nameh']
-    namei = request.POST['namei']
-    namej = request.POST['namej']
-    namek = request.POST['namek']
-    catername = request.POST['catername']
-    pricea = request.POST['pricea']
-    priceb = request.POST['priceb']
-    pricec = request.POST['pricec']
-    priced = request.POST['priced']
-    pricee = request.POST['pricee']
-    pricef = request.POST['pricef']
-    priceg = request.POST['priceg']
-    priceh = request.POST['priceh']
-    pricei = request.POST['pricei']
-    pricej = request.POST['pricej']
-    pricek = request.POST['pricek']
-    caterprice = request.POST['caterprice']
+    # nameb = request.POST['nameb']
+    # namec = request.POST['namec']
+    # named = request.POST['named']
+    # namee = request.POST['namee']
+    # namef = request.POST['namef']
+    # nameg = request.POST['nameg']
+    # nameh = request.POST['nameh']
+    # namei = request.POST['namei']
+    # namej = request.POST['namej']
+    # namek = request.POST['namek']
+    # catername = request.POST['catername']
+    # pricea = request.POST['pricea']
+    # priceb = request.POST['priceb']
+    # pricec = request.POST['pricec']
+    # priced = request.POST['priced']
+    # pricee = request.POST['pricee']
+    # pricef = request.POST['pricef']
+    # priceg = request.POST['priceg']
+    # priceh = request.POST['priceh']
+    # pricei = request.POST['pricei']
+    # pricej = request.POST['pricej']
+    # pricek = request.POST['pricek']
+    # caterprice = request.POST['caterprice']
 
     #Passing as a request TO the order.html page
     return render(request, "main/order.html", { 
+    # return HttpResponseRedirect(reverse("order", kwargs= {
       #On the left is the name of the variable on the order page
       'nameA': namea, 
-      'nameB': nameb,
-      'nameC': namec,
-      'nameD': named, 
-      'nameE': namee,
-      'nameF': namef,
-      'nameG': nameg, 
-      'nameH': nameh,
-      'nameI': namei,
-      'nameJ': namej, 
-      'nameK': namek,
-      'caterName': catername,
-      'priceA': pricea,
-      'priceB': priceb,
-      'priceC': pricec,
-      'priceD': priced,
-      'priceE': pricee,
-      'priceF': pricef,
-      'priceG': priceg,
-      'priceH': priceh,
-      'priceI': pricei,
-      'priceJ': pricej,
-      'priceK': pricek,
-      'caterPrice': caterprice
-    })
+      # 'nameB': nameb,
+      # 'nameC': namec,
+      # 'nameD': named, 
+      # 'nameE': namee,
+      # 'nameF': namef,
+      # 'nameG': nameg, 
+      # 'nameH': nameh,
+      # 'nameI': namei,
+      # 'nameJ': namej, 
+      # 'nameK': namek,
+      # 'caterName': catername,
+      # 'priceA': pricea,
+      # 'priceB': priceb,
+      # 'priceC': pricec,
+      # 'priceD': priced,
+      # 'priceE': pricee,
+      # 'priceF': pricef,
+      # 'priceG': priceg,
+      # 'priceH': priceh,
+      # 'priceI': pricei,
+      # 'priceJ': pricej,
+      # 'priceK': pricek,
+      # 'caterPrice': caterprice
+    })#) 
+    #Uncomment for switching to HttpRespRedir
+
+  #Need to keep this here so it's on the page it's supposed to be on, but how to redirect?
   return render(request, "main/admin5959.html")
 
 def send_email(request):
